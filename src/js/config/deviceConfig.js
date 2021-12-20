06967e69b09053f266f953aece84c96680b374af
Jacky <er0603@gmail.com>
2021-12-20 15:08:02 +0800

    chore (deviceConfig): modified the device config
    - Modified the device config, add new device uid.

--- src/js/config/deviceConfig.js
+++ src/js/config/deviceConfig.js
@@ -1,23 +1,26 @@
-export const deviceConfig = [{
-    "serialNumber": "e0d55e4d9bc6",
-    "name": "InnoAgent #1",
-    "reboot-button": true,
-    "power-button": true,
-    "gpio-button": true,
-    "tty-button": false,
-    "config-button": true,
-    "function-test": true,
-    "restart-button": true,
-},
-{
-    "serialNumber": "999A8D759B2A",
-    "name": "InnoAgent #2",
-    "reboot-button": true,
-    "power-button": true,
-    "gpio-button": true,
-    "tty-button": false,
-    "config-button": true,
-    "function-test": true,
-    "restart-button": true,
-}
+export const deviceConfig = [
+
+    {
+        "serialNumber": "48b02d50ebce",
+        "name": "InnoAgent #1",
+        "reboot-button": true,
+        "power-button": true,
+        "gpio-button": true,
+        "tty-button": false,
+        "config-button": true,
+        "function-test": true,
+        "restart-button": true,
+    },
+    {
+        "serialNumber": "e0d55e4d9bc6",
+        "name": "InnoAgent #2",
+        "reboot-button": true,
+        "power-button": true,
+        "gpio-button": true,
+        "tty-button": false,
+        "config-button": true,
+        "function-test": true,
+        "restart-button": true,
+    },
+
 ];
